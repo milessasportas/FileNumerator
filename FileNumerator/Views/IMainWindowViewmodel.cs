0b1bc210e16c2db8f88cbf906855431a3e5b6272
2 <2>
2019-04-08 00:06:01 +0200

    added some logic to the interface

--- FileNumerator/Views/IMainWindowViewmodel.cs
+++ FileNumerator/Views/IMainWindowViewmodel.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FileNumerator.Views
 {
-	public interface IMainWindowViewmodel
+	public interface IMainWindowViewmodel : INotifyPropertyChanged
 	{
+		/// <summary>
+		/// Command wich starts the Enumeration
+		/// </summary>
+		ICommand Start { get; }
+
+		/// <summary>
+		/// Coomand wich SelectsTheDirectory
+		/// </summary>
+		ICommand SelectDirectory { get; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		int StartNumber { get; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		int LastNumber { get; }
+
+		/// <summary>
+		/// The to ignored filetyped e.g. .pdf
+		/// </summary>
+		IEnumerable<string> IgnorFileType { get; }
+
+		/// <summary>
+		/// The to removed fileendings e.g. -final
+		/// </summary>
+		IEnumerable<string> DeleteFileendings { get; }
 
 	}
 }
