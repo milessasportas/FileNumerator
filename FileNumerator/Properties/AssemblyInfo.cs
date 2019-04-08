d0950dfeb402561444670ae0f8867e1ae72c0d1a
2 <2>
2019-04-08 19:36:55 +0200

    added a testproject

--- FileNumerator/Properties/AssemblyInfo.cs
+++ FileNumerator/Properties/AssemblyInfo.cs
@@ -4,6 +4,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+
+//for unit-testing
+[assembly: InternalsVisibleTo("FileNumeratorTests")]
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
---
+++ FileNumeratorTests/Viewmodels/MainWindowViewmodelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileNumerator.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNumerator.Viewmodels.Tests
+{
+	[TestClass()]
+	public class MainWindowViewmodelTests
+	{
+		[TestMethod()]
+		public void MainWindowViewmodelTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
