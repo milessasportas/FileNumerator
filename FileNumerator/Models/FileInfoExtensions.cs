d9ac768168d421f3c3005da777eb1785b8fefe67
Laptop <Laptop@flaptop.com>
2019-04-15 09:15:18 +0200

    added extension for FileInfo class

---
+++ FileNumerator/Models/FileInfoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNumerator.Models
+{
+	public static class FileInfoExtensions
+	{
+		/// <summary>
+		/// Wrapper for Path.GetFileNameWithoutExtension(fileInfo.FullName);
+		/// </summary>
+		/// <param name="fileInfo"></param>
+		/// <returns></returns>
+		public static string NameWithoutFileExtension(this FileInfo fileInfo)
+			=> Path.GetFileNameWithoutExtension(fileInfo.FullName);
+	}
+}
