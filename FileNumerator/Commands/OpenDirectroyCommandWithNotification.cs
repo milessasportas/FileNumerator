d4819d5565ffc422762080fc646fff2181ce1605
Laptop <Laptop@flaptop.com>
2019-04-18 09:56:01 +0200

    added OpenDirectroyCommandWithNotification command

---
+++ FileNumerator/Commands/OpenDirectroyCommandWithNotification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfHelperLibrary.Commands;
+
+namespace FileNumerator.Commands
+{
+    public class OpenDirectroyCommandWithNotification : OpenDirectoryCommand
+    {
+        public override string SelectedDirectory
+        {
+            set
+            {
+                if(SelectedDirectory != value)
+                {
+                    base.SelectedDirectory = value;
+                    DirectorySelected.Invoke(this, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// gets fired when the Directory gets selected by the user
+        /// </summary>
+        public event EventHandler<string> DirectorySelected;
+    }
+}
