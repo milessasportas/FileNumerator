5456c0e32aa2c5593863cdb39094b352d0e45d7b
Laptop <Laptop@flaptop.com>
2019-04-23 10:52:46 +0200

    made found files prettier

---
+++ FileNumerator/Converters/FilepathRemover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FileNumerator.Converters
+{
+    /// <summary>
+    /// Removes the Directory part of a Filepath
+    /// </summary>
+	public class FilepathRemover : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+            return value is string ? Path.GetFileName(value as string) : throw new NotSupportedException("Only string conversion supported.");
+		}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"/>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException("Only one way conversion supported");
+		}
+	}
+}
